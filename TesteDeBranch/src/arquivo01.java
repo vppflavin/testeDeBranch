e156da6ffc3af0a221d0be38eab28d4a426e5546
vppflavin <82177089+vppflavin@users.noreply.github.com>
2021-06-23 15:23:20 -0300

    declaração

--- TesteDeBranch/src/arquivo01.java
+++ TesteDeBranch/src/arquivo01.java
@@ -2,5 +2,6 @@ public class arquivo01 {
     public static void main(String[] args) {
         System.out.println("Ola nova branch");
         System.out.println("flavin mamaco");
+        System.out.println("ei gabriel coelho, queria saber se você se encontra solteiro");
     }
 }
