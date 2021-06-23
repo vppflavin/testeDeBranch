42155d6d9c921dcd9e22e07b39f58d45230949e1
VinteKls_Dev <81206911+20Kg-3s@users.noreply.github.com>
2021-06-23 15:24:24 -0300

    adicionado sistema de comparação.
    sistema de comparação que da imersão a mamaquice do colega de equipe

--- TesteDeBranch/src/arquivo01.java
+++ TesteDeBranch/src/arquivo01.java
@@ -2,5 +2,9 @@ public class arquivo01 {
     public static void main(String[] args) {
         System.out.println("Ola nova branch");
         System.out.println("flavin mamaco");
+        if(flavio = mamaco){
+            System.out.Print("Abra este link: https://www.youtube.com/watch?v=Fm2tmzLgr6Q")
+        }
+            
     }
 }
